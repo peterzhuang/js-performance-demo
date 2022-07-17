951ad1be6aa2f1c2da442355b1071364bfa2deee
Yuchuan <yuchuan@example.com>
2022-07-17 13:49:00 -0400

    solution 1 to optimize performance

--- rendering/moving-boxes/script.js
+++ rendering/moving-boxes/script.js
@@ -6,17 +6,17 @@ const {
  const elements = Array.from(document.querySelectorAll('.element'));
 
  registerNextClick(function (timestamp) {
-  // const nextPositions = elements.map(element => {
-  //   const top = element.offsetTop;
-  //   const nextPosition = +((Math.sin(top + timestamp/1000) + 1) / 2 * containerWidth);
-  //   return nextPosition;
-  // })
+  const nextPositions = elements.map(element => {
+    const top = element.offsetTop;
+    const nextPosition = +((Math.sin(top + timestamp/1000) + 1) / 2 * containerWidth);
+    return nextPosition;
+  })
 
 
   elements.forEach((element, index) => {
-    // const nextPosition = nextPositions[index];
-    const top = element.offsetTop;
-    const nextPosition = +((Math.sin(top + timestamp/1000) + 1) / 2 * containerWidth);
+    const nextPosition = nextPositions[index];
+    // const top = element.offsetTop;
+    // const nextPosition = +((Math.sin(top + timestamp/1000) + 1) / 2 * containerWidth);
     element.style.transform = `translateX(${nextPosition}px)`;
   });
  });
